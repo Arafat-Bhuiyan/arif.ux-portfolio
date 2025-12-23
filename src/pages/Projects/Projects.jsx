12c6ddebc68f35af62202671e65865f30aa7dcc2
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2025-12-23 11:26:13 +0600

    Explore all button added

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import miro from "../../assets/images/miro.svg";
 import photoshop from "../../assets/images/photoshop.svg";
 import figma from "../../assets/images/figma.svg";
-import logo from "../../assets/images/projects/learning-gpt-logo.png";
-import Home from "../../assets/images/projects/learning-home.png";
-import Topics from "../../assets/images/projects/learning-topics.png";
+import bgImg from "../../assets/images/blue-bg.png";
+import bgImg2 from "../../assets/images/blue-bg2.png";
 import card1logo from "../../assets/images/projects/card1logo.png";
 import card1home from "../../assets/images/projects/card1home.png";
 import card1topics from "../../assets/images/projects/card1topics.png";
@@ -38,113 +37,117 @@ import card10topics from "../../assets/images/projects/card10topics.png";
 
 import { Link } from "lucide-react";
 
-
 export const Projects = () => {
-    const projects = [
-  {
-    id: 1,
-    title: "Learnin GPT",
-    titleColor: "text-sky-500",
-    containerBg: "bg-sky-500/30",
-    innerBg: "bg-sky-700",
-    logo: card1logo,
-    image1: card1home,
-    image2: card1topics,
-  },
-  {
-    id: 2,
-    title: "A1c Boost",
-    titleColor: "text-yellow-400",
-    containerBg: "bg-teal-600/60",
-    innerBg: "bg-gradient-to-l from-sky-900 via-teal-600 to-cyan-950",
-    logo: card2logo,
-    image1: card2home,
-    image2: card2topics,
-  },
-  {
-    id: 3,
-    title: "Stars",
-    titleColor: "text-blue-600",
-    containerBg: "bg-sky-900/60",
-    innerBg: "bg-sky-800",
-    logo: card3logo,
-    image1: card3home,
-    image2: card3topics,
-  },
-  {
-    id: 4,
-    title: "J-Mart",
-    titleColor: "text-white",
-    containerBg: "bg-gradient-to-l from-sky-950 to-sky-700",
-    innerBg: "bg-blue-500",
-    logo: card4logo,
-    image1: card4home,
-    image2: card4topics,
-  },
-  {
-    id: 5,
-    title: "Scan 2Home",
-    titleColor: "text-red-500",
-    containerBg: "bg-[#3D2542]",
-    innerBg: "bg-[#7C3A54]",
-    logo: card5logo,
-    image1: card5home,
-    image2: card5topics,
-  },
-  {
-    id: 6,
-    title: "A1c Boost Landing Page",
-    titleColor: "text-yellow-400",
-    containerBg: "bg-teal-600/60",
-    innerBg: "bg-gradient-to-l from-sky-900 via-teal-600 to-cyan-950",
-    logo: card6logo,
-    image1: card6home,
-    image2: card6topics,
-  },
-  {
-    id: 7,
-    title: "The Radical Commons",
-    titleColor: "text-pink-900",
-    containerBg: "bg-pink-500",
-    innerBg: "bg-[#8E3F6F]",
-    logo: card7logo,
-    image1: card7home,
-    image2: card7topics,
-  },
-  {
-    id: 8,
-    title: "Farda",
-    titleColor: "text-white",
-    containerBg: "bg-emerald-700",
-    innerBg: "bg-sky-600",
-    logo: card8logo,
-    image1: card8home,
-    image2: card8topics,
-  },
-  {
-    id: 9,
-    title: "AI Real Estate Analysis",
-    titleColor: "text-green-600",
-    containerBg: "bg-gradient-to-r from-green-700 to-green-950",
-    innerBg: "bg-green-800",
-    logo: card9logo,
-    image1: card9home,
-    image2: card9topics,
-  },
-  {
-    id: 10,
-    title: "O-ber",
-    titleColor: "text-white",
-    containerBg: "bg-gradient-to-r from-sky-950 to-blue-500",
-    innerBg: "bg-sky-700",
-    logo: card10logo,
-    image1: card10home,
-    image2: card10topics,
-  },
-];
+  const projects = [
+    {
+      id: 1,
+      title: "Learnin GPT",
+      logoTitle: "Learnin GPT",
+      titleColor: "text-sky-500",
+      containerBg: "bg-sky-500/30",
+      innerBg: "bg-sky-700",
+      logo: card1logo,
+      image1: card1home,
+      image2: card1topics,
+    },
+    {
+      id: 2,
+      title: "A1c Boost",
+      logoTitle: "A1c Boost",
+      titleColor: "text-yellow-400",
+      containerBg: "bg-teal-600/60",
+      innerBg: "bg-gradient-to-l from-sky-900 via-teal-600 to-cyan-950",
+      logo: card2logo,
+      image1: card2home,
+      image2: card2topics,
+    },
+    {
+      id: 3,
+      title: "Stars",
+      titleColor: "text-blue-600",
+      containerBg: "bg-sky-900/60",
+      innerBg: "bg-sky-800",
+      logo: card3logo,
+      image1: card3home,
+      image2: card3topics,
+    },
+    {
+      id: 4,
+      title: "J-Mart",
+      titleColor: "text-white",
+      containerBg: "bg-gradient-to-l from-sky-950 to-sky-700",
+      innerBg: "bg-blue-500",
+      logo: card4logo,
+      image1: card4home,
+      image2: card4topics,
+    },
+    {
+      id: 5,
+      title: "Scan 2Home",
+      titleColor: "text-red-500",
+      containerBg: "bg-[#3D2542]",
+      innerBg: "bg-[#7C3A54]",
+      logo: card5logo,
+      image1: card5home,
+      image2: card5topics,
+    },
+    {
+      id: 6,
+      title: "A1c Boost Landing Page",
+      logoTitle: "A1c Boost",
+      titleColor: "text-yellow-400",
+      containerBg: "bg-teal-600/60",
+      innerBg: "bg-gradient-to-l from-sky-900 via-teal-600 to-cyan-950",
+      logo: card6logo,
+      image1: card6home,
+      image2: card6topics,
+    },
+    {
+      id: 7,
+      title: "The Radical Commons",
+      titleColor: "text-pink-900",
+      containerBg: "bg-pink-500",
+      innerBg: "bg-[#8E3F6F]",
+      logo: card7logo,
+      image1: card7home,
+      image2: card7topics,
+    },
+    {
+      id: 8,
+      title: "Farda",
+      logoTitle: "Farda",
+      titleColor: "text-white",
+      containerBg: "bg-emerald-700",
+      innerBg: "bg-sky-600",
+      logo: card8logo,
+      image1: card8home,
+      image2: card8topics,
+    },
+    {
+      id: 9,
+      title: "AI Real Estate Analysis",
+      titleColor: "text-green-600",
+      containerBg: "bg-gradient-to-r from-green-700 to-green-950",
+      innerBg: "bg-green-800",
+      logo: card9logo,
+      image1: card9home,
+      image2: card9topics,
+    },
+    {
+      id: 10,
+      title: "O-ber",
+      titleColor: "text-white",
+      containerBg: "bg-gradient-to-r from-sky-950 to-blue-500",
+      innerBg: "bg-sky-700",
+      logo: card10logo,
+      image1: card10home,
+      image2: card10topics,
+    },
+  ];
 
   return (
     <div className="pb-14">
+      {/* Header */}
       <div className="mb-32 space-y-2">
         <p className="self-stretch text-center justify-start text-sky-500 text-5xl font-semibold font-unbounded">
           Projects
@@ -163,7 +166,9 @@ export const Projects = () => {
             >
               <div className="w-full inline-flex justify-start items-center gap-20">
                 {/* Left Section */}
-                <div className={`w-1/2 h-[404px] relative ${project.innerBg} rounded-[75px] overflow-hidden`}>
+                <div
+                  className={`w-1/2 h-[404px] relative ${project.innerBg} rounded-[75px] overflow-hidden`}
+                >
                   <img
                     className="w-[310px] h-[313px] left-[232px] top-[30px] absolute object-cover object-center z-0"
                     src={project.image2}
@@ -176,8 +181,10 @@ export const Projects = () => {
                   />
                   <div className="left-[21px] top-[28px] absolute inline-flex justify-start items-center gap-2.5">
                     <img className="" src={project.logo} alt="logo" />
-                    <p className={`justify-start text-white text-xl font-bold font-unbounded ${project.titleColor}`}>
-                      {project.title}
+                    <p
+                      className={`justify-start text-white text-xl font-bold font-unbounded ${project.titleColor}`}
+                    >
+                      {project.logoTitle}
                     </p>
                   </div>
                 </div>
@@ -188,7 +195,9 @@ export const Projects = () => {
                     <div className="w-96 flex flex-col justify-start items-start gap-5">
                       <div className="self-stretch flex flex-col justify-start items-start gap-5">
                         <div className="flex flex-col justify-start items-start gap-5">
-                          <p className={`justify-start text-4xl font-medium font-unbounded ${project.titleColor}`}>
+                          <p
+                            className={`justify-start text-4xl font-medium font-unbounded ${project.titleColor}`}
+                          >
                             {project.title}
                           </p>
                           <p className="justify-start text-white text-2xl font-normal font-unbounded">
@@ -251,6 +260,15 @@ export const Projects = () => {
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="flex justify-center py-20">
+          <button
+            type="button"
+            className="w-[487px] h-20 bg-sky-500/30 rounded-[119px] outline outline-2 outline-offset-[-2px] outline-sky-500 backdrop-blur-sm text-white text-2xl font-normal font-['Unbounded'] flex items-center justify-center hover:bg-sky-500/40 transition"
+          >
+            Explore All
+          </button>
         </div>
       </div>
     </div>
