cae5fff10eb8d7a05143bc307b363e276c206499
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2025-12-23 10:34:06 +0600

    Projects component added and json file created and added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 import { About } from "../About/About";
+import { Projects } from "../Projects/Projects";
 
 export const Home = () => {
   return (
     <div>
       <Welcome />
       <About />
+      <Projects />
     </div>
   );
 };
